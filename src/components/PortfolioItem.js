2d9bedf651f1b8475f0c00680df04e4b29c216cb
Michael <Mick666@hotmail.com.au>
2020-11-18 04:07:22 +1100

    Added file structure

---
+++ src/components/PortfolioItem.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ImageGallery from 'react-image-gallery'
+
+const PortfolioItem = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default PortfolioItem
