a963efe6aa89fe383200782e2eb36acfc4322fa3
Michael <Mick666@hotmail.com.au>
2021-05-01 20:08:42 +1000

    Reworked Project data

--- src/portfolioData.js
+++ src/portfolioData.js
@@ -1,35 +1,6 @@
 import MP1 from './images/MPImp-1.jpg'
-import MP1Thumb from './images/MPImp-1-Thumb.jpg'
-import MP2 from './images/MPImp-2.jpg'
-import MP2Thumb from './images/MPImp-2-Thumb.jpg'
-import MP3 from './images/MPImp-3.jpg'
-import MP3Thumb from './images/MPImp-3-Thumb.jpg'
-import MP4 from './images/MPImp-4.jpg'
-import MP4Thumb from './images/MPImp-4-Thumb.jpg'
-import MP5 from './images/MPImp-5.jpg'
-import MP5Thumb from './images/MPImp-4-Thumb.jpg'
 import Aghs1 from './images/Aghs1.jpg'
-import Aghs1Thumb from './images/Aghs1-Thumb.jpg'
-import Aghs2 from './images/Aghs2.jpg'
-import Aghs2Thumb from './images/Aghs2-Thumb.jpg'
-import Aghs3 from './images/Aghs3.jpg'
-import Aghs3Thumb from './images/Aghs3-Thumb.jpg'
-import Aghs4 from './images/Aghs4.jpg'
-import Aghs4Thumb from './images/Aghs4-Thumb.jpg'
-import Aghs5 from './images/Aghs5.jpg'
-import Aghs5Thumb from './images/Aghs5-Thumb.jpg'
-import Aghs6 from './images/Aghs6.jpg'
-import Aghs6Thumb from './images/Aghs6-Thumb.jpg'
 import Linker1 from './images/Linker1.jpg'
-import Linker1Thumb from './images/Linker1-Thumb.jpg'
-import Linker2Thumb from './images/Linker2-Thumb.jpg'
-import Linker2 from './images/Linker2.jpg'
-import Commands1 from './images/Commands1.jpg'
-import Commands1Thumb from './images/Commands1-Thumb.jpg'
-import Commands2Thumb from './images/Commands2-Thumb.jpg'
-import Commands2 from './images/Commands2.jpg'
-import Calc1 from './images/Calc1.jpg'
-import Calc1Thumb from './images/Calc1-Thumb.jpg'
 
 
 const Data = [
@@ -37,10 +8,10 @@ const Data = [
         title: 'Aghanim\'s Pathfinders Guides',
         image: Aghs1,
         text: [
-            'A website for a popular mod for Dota 2, Aghanim’s Pathfinders.',
-            'The site hosts player-made guides, showcases recent victories and displays a wide range of statistics on the mod\'s heroes',
+            'A website for a popular mod for Dota 2, Aghanim’s Pathfinders. The website is heavily used by the playerbase and has been integrated with the mod by the developer to provide post-game screens',
+            'The site hosts player-made guides, showcases recent victories and displays a wide range of statistics on the mod\'s heroes. It uses React Router to display multiple pages while remaining a single page app.',
             'The website features: ',
-            ['Hover elements', 'Live stats from the game\'s server', 'Complex guide creation page', 'Detailed post-game screens'],
+            ['Live stats from the game\'s server', 'A guide creation page', 'Detailed post-game screens', 'Detailed information hovering over certain elements'],
             'Tech stack:',
             ['React', 'Node.js', 'GraphQL', 'MongoDB', 'CSS3']
         ],
@@ -49,20 +20,27 @@ const Data = [
     },
     {
         title: 'Mediaportal Improvements',
-        image: MP4,
-        text: ['A Chrome extension I created to assist with creating Daily Briefings while at iSentia. \
-            The extension fixes several issues my colleagues had with the site we used, Mediaportal,\
-            and added several time saving features.\n The extension now has more than 30 users across three\
-            teams and has been used in conjunction with the Mediaportal developers to eliminate an ongoing\
-            bug we were experiencing.\n'],
+        image: MP1,
+        text: [
+            'I created this Chrome extension to assist iSentia\'s Daily Briefing\'s Department with their website they use to create products for clients, Mediaportal.',
+            'Originally, it aimed only at fixing several long-standing bugs and complaints the department had with the site (as it is designed for clients, not the fepartment).',
+            'However, the extension has continually expanded over the past 12 months and has become essential in delivering products to several clients, such as the Department of Home Affairs, Services Australia and Coles',
+            'It has more than 30 users and has gone from unofficial project to required across the entire Department. Some of the productivity gains have seen more than an hour removed for some briefings, while the extension\'s checking tool has entirely eliminated user errors in our products',
+            'Tech stack:',
+            ['HTML', 'Vanilla JS', 'CSS3']
+        ],
         live: 'https://chrome.google.com/webstore/detail/mp-improvements/fehdcpnecldplanhfcmhcanpagpmljmo',
         source: 'https://github.com/Mick666/Media-Plugin'
     },
     {
         title: 'Summary Linker',
         image: Linker1,
-        text: ['A website designed to assist with linking news summaries when using our online editing tool.\
-        Created with React.'],
+        text: [
+            'A simple website created for iSentia\'s Daily Briefings team to assist with hyperlinking briefings.',
+            'A user enters their news summaries and links and the site returns them linked and styled according to the options in the sidebar',
+            'Tech stack:',
+            ['HTML', 'Vanilla JS', 'CSS3']
+        ],
         live: 'https://mick666.github.io/Summs-Linker-Redux/',
         source: 'https://github.com/Mick666/Summs-Linker-Redux'
     },
